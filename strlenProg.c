6f61b8c4d7f4a70401618e2747c8958eb2baedcf
Aakash Kshettry <bluepulse1989@gmail.com>
2019-02-26 22:13:10 +0545

    Add files via upload

---
+++ strlenProg.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	char word[50];
+	int len,i;
+	len=0;
+	printf("Enter the word");
+	scanf("%[^\n]s",word);				//taking the input as a string in word
+	for (i=0;word[i]!='\0';i++)
+	{
+		len++;
+	}
+	printf("%d",len);
+	return 0;
+}
