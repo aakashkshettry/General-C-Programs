454cbf7278c1ec1b7765ae0bcf12f2b94acd580e
Aakash Kshettry <bluepulse1989@gmail.com>
2019-03-26 07:00:25 +0545

    Update hcflcm.c

--- hcflcm.c
+++ hcflcm.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,temp,r;
+	int a,b,temp,r,hcf,lcm,firstNumber,secondNumber;
 	printf("Enter two numbers ");
 	scanf("%d%d",&a,&b);
+	firstNumber=a;
+	secondNumber=b;
 	if (a>b)
 	{
 		temp=a;
@@ -16,6 +18,9 @@ int main()
 		a=r;
 		b=a;
 	}
-	printf("HCF=%d",a);
+	hcf=a;
+	lcm=(firstNumber*secondNumber)/hcf;
+	printf("HCF=%d",hcf);
+	printf("LCM=%d",lcm);
 	return 0;
 }
